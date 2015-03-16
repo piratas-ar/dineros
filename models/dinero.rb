a6ad4c4518256169dacc05621bf467095a384589
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-03-15 22:11:47 -0300

    upgrade, aunque falta migrar a padrino 0.12

--- models/dinero.rb
+++ models/dinero.rb
@@ -2,10 +2,10 @@
 class Dinero < ActiveRecord::Base
   timestamps
   validates_format_of :responsable,
-                      :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/,
-                      :on => :create
+                      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/,
+                      on: :create
 
-  property :cantidad, :as => :integer, :null => false
-  property :responsable, :null => false, :index => true
-  property :comentario, :as => :text
+  property :cantidad, as: :integer
+  property :responsable, index: true
+  property :comentario, as: :text
 end
