b834215de97a0b384a25fbf2c5b175675ce7ab88
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-03-29 16:08:42 -0300

    usar word_wrap en los mails en texto plano

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,7 @@ module Dineros
     register Padrino::Rendering
     register Padrino::Mailer
     register Padrino::Helpers
+    register Padrino::Helpers::FormatHelpers
     register Kaminari::Helpers::SinatraHelpers
 
     enable :sessions
