a159209c38ab35c5e59756155a5d2a1a9882aa35
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-03-31 16:54:52 -0300

    deployear guardando la base de datos y gpg

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
 set :application, 'dineros'
 set :repo_url, 'http://repo.hackcoop.com.ar/dineros.git'
 
+set :linked_dirs, %w{db gnupg}
+set :linked_files, %w{.env}
+
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.0'
+set :rbenv_ruby, '2.2.1'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
@@ -11,6 +14,12 @@ set :rbenv_roles, :all # default value
 namespace :deploy do
   desc "Genera la llave gpg remota"
   task :gpg_generate do
-    run :rake, 'gpg:generate'
+    on primary :app do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'gpg:generate'
+        end
+      end
+    end
   end
 end
---
+++ config/deploy/edsl.rb
@@ -0,0 +1,16 @@
+set :deploy_user, 'app'
+set :deploy_to, '/srv/http/dineros.endefensadelsl.org'
+set :branch, 'develop'
+
+set :rails_env, 'production'
+
+# en Parabola /tmp está montado noexec
+set :tmp_dir, "#{fetch :deploy_to}/tmp"
+
+# Las gemas se comparten con otras apps
+set :bundle_path, '/srv/http/gemas'
+# Evitar compilar nokogiri
+set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
+
+# IP del VPS
+server 'lainventoria.com.ar', port: 22, user: fetch(:deploy_user), roles: %w{app web db}
