1e1a37184906d9a142b030ff4b33110be9216eb2
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-07-22 16:47:28 -0300

    hacer backups!

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,22 @@ namespace :deploy do
   task :gpg_generate do
     on primary :app do
       within release_path do
-        with rails_env: fetch(:rails_env) do
+        with padrino_env: fetch(:rails_env) do
           execute :rake, 'gpg:generate'
+        end
+      end
+    end
+  end
+
+  desc 'Hace un backup de la base de datos antes de migrarla'
+  task :backup, [:set_rails_env] do
+    on primary :db do
+      within release_path do
+        with padrino_env: fetch(:rails_env) do
+          # https://gist.github.com/rsutphin/9010923
+          resolved_release_path = capture(:pwd, "-P")
+          set(:release_name, resolved_release_path.split('/').last)
+          execute :cp, "db/dineros_#{fetch(:rails_env)}.db db/dineros_pre_#{fetch(:release_name)}.db"
         end
       end
     end
@@ -34,3 +48,6 @@ namespace :deploy do
     end
   end
 end
+
+before 'deploy:mr_migrate', 'deploy:backup'
+before 'deploy:restart', 'deploy:mr_migrate'
