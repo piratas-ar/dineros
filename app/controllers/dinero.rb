2846bfa3f4f76f3a6c34921e5a3568dab56fdf90
aza <fake@email.com>
2015-09-10 20:10:16 -0300

    quitando validacion de monto en la transferencia

--- app/controllers/dinero.rb
+++ app/controllers/dinero.rb
@@ -65,42 +65,37 @@ Dineros::App.controllers :dinero do
   post :transferir  do
     cantidad = (params[:dinero][:cantidad].to_f * 100).to_i
     if cantidad > 0
-      antes_de_transferir = Dinero.where(responsable: params[:dinero][:responsable_entrega]).where(moneda: params[:dinero][:moneda]).sum(:cantidad)
-      if antes_de_transferir > cantidad
-        @dinero_entrega = Dinero.new
-    # TODO esto podría ir en la validación del modelo...
-        # numero de 2 decimales, se eliminan los decimales sobrantes
-        # no se redondea
-        @dinero_entrega.cantidad = cantidad * -1
-        @dinero_entrega.moneda = params[:dinero][:moneda]
-        @dinero_entrega.responsable = params[:dinero][:responsable_entrega]
+      @dinero_entrega = Dinero.new
+  # TODO esto podría ir en la validación del modelo...
+      # numero de 2 decimales, se eliminan los decimales sobrantes
+      # no se redondea
+      @dinero_entrega.cantidad = cantidad * -1
+      @dinero_entrega.moneda = params[:dinero][:moneda]
+      @dinero_entrega.responsable = params[:dinero][:responsable_entrega]
 
-        @dinero_recibe = Dinero.new
-    # TODO esto podría ir en la validación del modelo...
-        # numero de 2 decimales, se eliminan los decimales sobrantes
-        # no se redondea
-        @dinero_recibe.cantidad = cantidad
-        @dinero_recibe.moneda = params[:dinero][:moneda]
-        @dinero_recibe.responsable = params[:dinero][:responsable_recibe]
+      @dinero_recibe = Dinero.new
+  # TODO esto podría ir en la validación del modelo...
+      # numero de 2 decimales, se eliminan los decimales sobrantes
+      # no se redondea
+      @dinero_recibe.cantidad = cantidad
+      @dinero_recibe.moneda = params[:dinero][:moneda]
+      @dinero_recibe.responsable = params[:dinero][:responsable_recibe]
 
-        @dinero_entrega.comentario = @dinero_entrega.nombre.concat(' >> ').concat(@dinero_recibe.nombre).concat(': ').concat(params[:dinero][:comentario])
-        @dinero_recibe.comentario = @dinero_entrega.comentario
+      @dinero_entrega.comentario = @dinero_entrega.nombre.concat(' >> ').concat(@dinero_recibe.nombre).concat(': ').concat(params[:dinero][:comentario])
+      @dinero_recibe.comentario = @dinero_entrega.comentario
 
-        # valida que la persona que transfiere tenga lo que va a transferir
-        if @dinero_entrega.save
-          if @dinero_recibe.save
-            deliver :dineros, :movimiento, @dinero_entrega
-            deliver :dineros, :movimiento, @dinero_recibe
-            redirect '/'
-          else
-            @dinero_entrega.delete
-            'Hubo un error'
-          end
+      # valida que la persona que transfiere tenga lo que va a transferir
+      if @dinero_entrega.save
+        if @dinero_recibe.save
+          deliver :dineros, :movimiento, @dinero_entrega
+          deliver :dineros, :movimiento, @dinero_recibe
+          redirect '/'
         else
+          @dinero_entrega.delete
           'Hubo un error'
         end
       else
-        'No tiene suficiente dinero para transferir'
+        'Hubo un error'
       end
     else
       'La cantidad debe ser un número positivo'
