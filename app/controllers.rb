a00de4c7b5156f6022eac46a45a010b6a96f1bf3
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-12-30 19:00:12 -0300

    mostrar un balance

--- app/controllers.rb
+++ app/controllers.rb
@@ -3,22 +3,25 @@ Dineros::App.controllers  do
 # Obtener todo el historial
     @dineros = Dinero.all.order(created_at: :desc).page(params[:page]).per(params[:limit] || 10)
 # Mostrar el total
-    @monedas = Dinero.group(:moneda).pluck(:moneda)
     @total = Dinero.sum(:cantidad)
-    @totales = []
-    @monedas.each do |moneda|
-      total = Dinero.where(moneda: moneda).sum(:cantidad)
-      @totales << Money.new(total, moneda) unless total == 0
-    end
-# Obtener un listado de los que tienen plata en este momento
-    @quien_los_tiene = Dinero.group(:responsable).group(:moneda).
-      select([:responsable, :moneda, 'sum(cantidad) as subtotal']).
-      having('subtotal <> 0')
 
-# Convertir los subtotales a monedas
-    @quien_los_tiene.collect do |q|
-      q.subtotal = Money.new(q.subtotal, q.moneda)
-    end
+    # Obtener una tabla así:
+    #
+    # | Moneda | Responsable | Ingreso | Egreso | Balance |
+    # +--------+-------------+---------+--------+---------+
+    # | ARS    | fauno       | 3000    | 300    | 2700    |
+    # | BTC    | fauno       |    1    |   0    |    1    |
+    @balances = Dinero.group(:moneda).group(:responsable).
+      select([ :responsable, :moneda,
+      'sum(case when cantidad > 0 then cantidad else 0 end) as ingreso',
+      'sum(case when cantidad < 0 then cantidad else 0 end) as egreso',
+      'sum(cantidad) as cantidad' ])
+
+    @balances_resumen = Dinero.group(:moneda).
+      select([ :responsable, :moneda,
+      'sum(case when cantidad > 0 then cantidad else 0 end) as ingreso',
+      'sum(case when cantidad < 0 then cantidad else 0 end) as egreso',
+      'sum(cantidad) as cantidad' ])
 
 # Incluir funciones para gravatar
 # TODO: usar avatars.io para encontrar avatares en otros servicios?
