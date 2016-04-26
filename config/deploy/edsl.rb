89dfb1c6eacbeb37871b183a8b982b3fd3c24aa9
Nicolás Reynolds <fauno@endefensadelsl.org>
2016-04-26 20:53:55 -0300

    edsl tambien usa passenger

--- config/deploy/edsl.rb
+++ config/deploy/edsl.rb
@@ -12,5 +12,11 @@ set :bundle_path, '/srv/http/gemas'
 # Evitar compilar nokogiri
 set :bundle_env_variables, { nokogiri_use_system_libraries: 1 }
 
+set :default_env, path: '/usr/lib/passenger/bin/:$PATH'
+
+set :passenger_restart_with_sudo, false
+set :passenger_environment_variables, path: '/usr/lib/passenger/bin/:$PATH'
+set :passenger_restart_command, 'sudo passenger-config restart-app'
+
 # IP del VPS
 server 'lainventoria.com.ar', port: 22, user: fetch(:deploy_user), roles: %w{app web db}
