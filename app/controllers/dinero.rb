7d113d37a51d29023e7c9c677fa65fb4ff9800d1
Nicolás Reynolds <fauno@endefensadelsl.org>
2016-04-29 13:27:51 -0300

    refactorizando

--- app/controllers/dinero.rb
+++ app/controllers/dinero.rb
@@ -62,17 +62,17 @@ Dineros::App.controllers :dinero do
   end
 
   post :transferir do
-    params[:dinero][:cantidad] = (params[:dinero][:cantidad].to_f * 100).to_i
+    params[:dinero]['cantidad'] = (params[:dinero][:cantidad].to_f * 100).to_i
     halt 'La cantidad debe ser un número positivo' unless params[:dinero][:cantidad] > 0
 
     recibe  = params[:dinero]
     entrega = recibe.dup
-    entrega[:responsable] = entrega.delete("responsable_entrega")
-    recibe[:responsable]  = recibe.delete("responsable_recibe")
-    entrega[:cantidad]    = entrega[:cantidad] * -1
+    entrega['responsable'] = entrega.delete('responsable_entrega')
+    recibe['responsable']  = recibe.delete('responsable_recibe')
+    entrega['cantidad']    = entrega[:cantidad] * -1
 
-    recibe.delete("responsable_entrega")
-    entrega.delete("responsable_recibe")
+    recibe.delete('responsable_entrega')
+    entrega.delete('responsable_recibe')
 
     dineros = [ Dinero.new(entrega), Dinero.new(recibe) ]
 
@@ -82,6 +82,7 @@ Dineros::App.controllers :dinero do
     saved = []
     Dinero.transaction do
       saved = dineros.map(&:save)
+      raise ActiveRecord::Rollback unless saved.all?
     end
 
     if saved.all?
@@ -107,18 +108,21 @@ Dineros::App.controllers :dinero do
   # cosa.  En este caso se registra un movimiento de salida de lo que la
   # pirata tenía y uno de entrada.
   post :intercambiar do
-    dineros = []
-    dineros << dinero  = Dinero.new
-    dinero.cantidad    = (params[:dinero][:cantidad].to_f * 100).to_i * -1
-    dinero.moneda      = params[:dinero][:moneda]
-    dinero.responsable = params[:dinero][:responsable]
-    dinero.comentario  = params[:dinero][:comentario]
+    params[:dinero]['cantidad'] = (params[:dinero][:cantidad].to_f * 100).to_i
+    params[:dinero]['cantidad_recibida'] = (params[:dinero][:cantidad_recibida].to_f * 100).to_i
 
-    dineros << recibido  = Dinero.new
-    recibido.cantidad    = (params[:dinero][:cantidad_recibida].to_f * 100).to_i
-    recibido.moneda      = params[:dinero][:moneda_recibida]
-    recibido.responsable = params[:dinero][:responsable]
-    recibido.comentario  = params[:dinero][:comentario]
+    recibido             = params[:dinero]
+    responsable_opcional = recibido.delete('responsable_opcional')
+    intercambio_interno  = !responsable_opcional.empty? && (recibido[:responsable] != responsable_opcional)
+    entregado            = recibido.dup
+
+    entregado.delete('cantidad_recibida')
+    entregado.delete('moneda_recibida')
+    entregado['cantidad'] = entregado[:cantidad] * -1
+    recibido['cantidad']  = recibido.delete('cantidad_recibida')
+    recibido['moneda']    = recibido.delete('moneda_recibida')
+
+    dineros = [ Dinero.new(entregado), Dinero.new(recibido) ]
 
     # Se hizo un intercambio dentro del sistema.  Si doy 100 ARS a
     # cambio de 1 BTC, hay que registrar:
@@ -127,19 +131,25 @@ Dineros::App.controllers :dinero do
     # * vos: -1 BTC   -- dinero_inverso
     # * yo: 1 BTC     -- recibido
     # * vos: 100 ARS  -- recibido_inverso
-    if !params[:dinero][:responsable_opcional].empty?
-      dineros << dinero_inverso  = recibido.dup
-      dinero_inverso.cantidad    = dinero_inverso.cantidad * -1
-      dinero_inverso.responsable = params[:dinero][:responsable_opcional]
+    if intercambio_interno
+      entregado_inverso = recibido.dup
+      recibido_inverso  = entregado.dup
 
-      dineros << recibido_inverso  = dinero.dup
-      recibido_inverso.cantidad    = recibido_inverso.cantidad * -1
-      recibido_inverso.responsable = params[:dinero][:responsable_opcional]
+      entregado_inverso['cantidad']    = recibido[:cantidad] * -1
+      entregado_inverso['responsable'] = responsable_opcional
+
+      recibido_inverso['cantidad']    = entregado[:cantidad] * -1
+      recibido_inverso['responsable'] = responsable_opcional
+
+      dineros << Dinero.new(entregado_inverso)
+      dineros << Dinero.new(recibido_inverso)
     end
 
     saved = []
     Dinero.transaction do
       saved = dineros.map(&:save)
+
+      raise ActiveRecord::Rollback unless saved.all?
     end
 
     if saved.all?
