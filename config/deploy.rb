e8f652158ad75f89f6b92e8afd1ff89fc53ce519
fauno <fauno@endefensadelsl.org>
2016-08-20 21:16:01 -0300

    no usar rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,6 @@ set :repo_url, 'https://github.com/piratas-ar/dineros'
 set :linked_dirs, %w(db gnupg)
 set :linked_files, %w(.env public/dineros.asc)
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.2.1'
-
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w(rake gem bundle ruby rails)
-set :rbenv_roles, :all # default value
-
 namespace :deploy do
   desc 'Genera la llave gpg remota'
   task :gpg_generate do
