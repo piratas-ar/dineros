a703899d114c8e2834a4a617e49dc2240e97b46e
fauno <fauno@endefensadelsl.org>
2016-10-16 02:09:44 -0300

    filtrar por fecha y responsable

--- app/controllers.rb
+++ app/controllers.rb
@@ -4,10 +4,17 @@ Dineros::App.controllers do
 
     # Filtros
     [ :grupo, :moneda ].each do |filtro|
-      unless params[filtro].blank?
-        instance_variable_set('@' + filtro.to_s, params[filtro])
-        @dineros = @dineros.where(Hash[filtro, params[filtro]])
-      end
+      next if params[filtro].blank?
+
+      instance_variable_set('@' + filtro.to_s, params[filtro])
+      @dineros = @dineros.where(Hash[filtro, params[filtro]])
+    end
+
+    [ :created_at, :responsable ].each do |filtro_empieza_por|
+      next if params[filtro_empieza_por].blank?
+
+      instance_variable_set('@' + filtro_empieza_por.to_s, params[filtro_empieza_por])
+      @dineros = @dineros.where("#{filtro_empieza_por} like :valor", valor: params[filtro_empieza_por] + '%')
     end
 
     # Obtener todo el historial
